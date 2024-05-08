a5bdd3a7b6721ceb7b0488366cdd6fae64fd5ddc
Harry Commin <harry.commin@enclustra.com>
2024-05-08 10:36:54 +0200

    Move to MIT license

--- hdl/en_cl_fix_private_pkg.vhd
+++ hdl/en_cl_fix_private_pkg.vhd
@@ -1,18 +1,31 @@
 ---------------------------------------------------------------------------------------------------
--- Copyright (c) 2022 Enclustra GmbH, Switzerland (info@enclustra.com)
+-- Copyright (c) 2024 Enclustra GmbH, Switzerland (info@enclustra.com)
+-- 
+-- Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+-- and associated documentation files (the "Software"), to deal in the Software without
+-- restriction, including without limitation the rights to use, copy, modify, merge, publish,
+-- distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the
+-- Software is furnished to do so, subject to the following conditions:
+-- 
+-- The above copyright notice and this permission notice shall be included in all copies or
+-- substantial portions of the Software.
+-- 
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+-- BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+-- NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+-- DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+-- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 ---------------------------------------------------------------------------------------------------
 
 ---------------------------------------------------------------------------------------------------
 -- Libraries
 ---------------------------------------------------------------------------------------------------
-
 library ieee;
     use ieee.std_logic_1164.all;
 
 ---------------------------------------------------------------------------------------------------
 -- Package Header
 ---------------------------------------------------------------------------------------------------
-
 package en_cl_fix_private_pkg is
     
     function choose(condition : boolean; if_true : integer; if_false : integer) return integer;
@@ -33,7 +46,6 @@ end package;
 ---------------------------------------------------------------------------------------------------
 -- Package Body
 ---------------------------------------------------------------------------------------------------
-
 package body en_cl_fix_private_pkg is
     
     function choose(condition : boolean; if_true : integer; if_false : integer) return integer is
@@ -193,7 +205,7 @@ package body en_cl_fix_private_pkg is
         -- Checks
         assert StartIdx <= Str'high and StartIdx >= Str'low report "string_parse_int: StartIdx out of range" severity Failure;
         
-        -- remove leading spaces
+        -- Remove leading spaces
         while Str(CurrentIdx_v) = ' ' loop
             CurrentIdx_v := CurrentIdx_v + 1;
         end loop;
